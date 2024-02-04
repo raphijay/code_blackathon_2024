841f148d7e074da89cb86faf4a61252f31af63e8
raphijay <87781609+raphijay@users.noreply.github.com>
2024-02-04 01:34:25 -0500

    added more api calls

--- blackathon_2024/blackathon_2024/views.py
+++ blackathon_2024/blackathon_2024/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
 from .models import Tags, Perspectives, Events
 from .serializers import *
+from rest_framework.parsers import JSONParser
 
 @csrf_exempt
 @require_http_methods(["POST"])
@@ -39,3 +40,76 @@ def oppose(request, pk):
                 return HttpResponse(status=400)
         except Perspectives.DoesNotExist:
             return HttpResponse(status=404)
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def addperspective(request):
+
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = PerspectivesSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+@csrf_exempt
+@require_http_methods(["POST"])
+def addevent(request):
+
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = EventsSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+@csrf_exempt
+@require_http_methods(["GET"])
+def getperspectives_all(request):
+
+    if request.method == "GET":
+        things = Perspectives.objects.all()
+        serializer = PerspectivesSerializer(things, many=True)
+
+        return JsonResponse(serializer.data)
+    
+
+@csrf_exempt
+@require_http_methods(["GET"])
+def getevents_all(request):
+
+    if request.method == "GET":
+        things = Events.objects.all()
+        serializer = EventsSerializer(things, many=True)
+
+        return JsonResponse(serializer.data)
+    
+@csrf_exempt
+@require_http_methods(["GET"])
+def perspective_choice(request, pk):
+
+    try:
+        perspective = Perspectives.objects.get(pk=pk)
+    except Perspectives.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = PerspectivesSerializer(perspective)
+        return JsonResponse(serializer.data)
+    
+
+@csrf_exempt
+@require_http_methods(["GET"])
+def event_choice(request, pk):
+
+    try:
+        event = Events.objects.get(pk=pk)
+    except Events.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = EventsSerializer(event)
+        return JsonResponse(serializer.data)
+
